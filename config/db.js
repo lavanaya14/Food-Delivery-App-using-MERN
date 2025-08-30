7de7a8bef8e5067e6ff8cbcf3421793ebcfaac1a
Lavanaya Rajput <149952338+lavanaya14@users.noreply.github.com>
2025-08-30 23:24:42 +0530

    Delete config directory

--- config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://lavanayarajput:BRavEsToNE@cluster0.m4mgkx9.mongodb.net/food-del').then(() => console.log("DB Connected"));
-}
